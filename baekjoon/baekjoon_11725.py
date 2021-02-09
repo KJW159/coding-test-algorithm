5985b5c1db0b140ffeb571f69c35111fffdab626
KJW <dnnls1@naver.com>
2021-02-09 15:17:09 +0900

    210209 v2

--- baekjoon/baekjoon_11725.py
+++ baekjoon/baekjoon_11725.py
@@ -174,33 +174,92 @@
 import collections
 import sys
 
-def bfs():
-    queue = collections.deque()
-    queue.append(1)
+# def bfs():
+#     queue = collections.deque()
+#     queue.append(1)
+#
+#     visited = [0] * (N + 1)
+#     visited[1] = 1
+#
+#     while queue:
+#         node = queue.popleft()
+#         for curr in adj_list[node]:
+#             if visited[curr] == 0:
+#                 parents[curr] = node
+#                 queue.append(curr)
+#                 visited[curr] = 1
+#
+#
+# N = int(input())
+# adj_list = [[] for _ in range(N+1)]
+# res = 0
+# parents = [0]*(N+1)
+#
+# for __ in range(N-1):
+#     n1, n2 = map(int, sys.stdin.readline().split())
+#     adj_list[n1].append(n2)
+#     adj_list[n2].append(n1)
+#
+# bfs()
+#
+# for i in range(2, N+1):
+#     print(parents[i])
 
-    visited = [0] * (N + 1)
-    visited[1] = 1
+# re-v1
+# from collections import deque
+#
+# def finding_parents():
+#     queue = deque()
+#     queue.append(1)
+#     parents[1] = 1
+#     while queue:
+#         node = queue.popleft()
+#         for child in adj_list[node]:
+#             if parents[child] == 0:
+#                 parents[child] = node
+#                 queue.append(child)
+#
+#
+# N = int(input())
+# adj_list = [[] for _ in range(N+1)]
+#
+# parents = [0]*(N+1)
+# for _ in range(N-1):
+#     u, v = map(int, input().split())
+#     adj_list[u].append(v)
+#     adj_list[v].append(u)
+#
+# finding_parents()
+#
+# # for p in parents[2:]:
+# #     print(p)
+#
+# for i in range(2,N+1):
+#     print(parents[i])
 
-    while queue:
-        node = queue.popleft()
-        for curr in adj_list[node]:
-            if visited[curr] == 0:
-                parents[curr] = node
-                queue.append(curr)
-                visited[curr] = 1
+# re-v2
+def finding_parents_dfs():
+    stack = []
+    stack.append(1)
+    parents[1] = 1
+    while stack:
+        node = stack.pop()
+        for child in adj_list[node]:
+            if parents[child] == 0:
+                stack.append(child)
+                parents[child] = node
 
 
 N = int(input())
 adj_list = [[] for _ in range(N+1)]
-res = 0
+
 parents = [0]*(N+1)
+for _ in range(N-1):
+    u, v = map(int, input().split())
+    adj_list[u].append(v)
+    adj_list[v].append(u)
 
-for __ in range(N-1):
-    n1, n2 = map(int, sys.stdin.readline().split())
-    adj_list[n1].append(n2)
-    adj_list[n2].append(n1)
+finding_parents_dfs()
 
-bfs()
-
-for i in range(2, N+1):
+for i in range(2,N+1):
     print(parents[i])
