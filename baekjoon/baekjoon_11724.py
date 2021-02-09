d8b05e2cfb26724079b30ba8ae76a350fc4f2e1d
KJW <dnnls1@naver.com>
2021-02-09 11:35:11 +0900

    210209 v2

--- baekjoon/baekjoon_11724.py
+++ baekjoon/baekjoon_11724.py
@@ -29,18 +29,41 @@ import collections
 
 # re-v1
 
+# def dfs(start):
+#     stack = []
+#     stack.append(start)
+#     visited[start] = 1
+#
+#     while stack:
+#         start = stack.pop()
+#         for node in adj_list[start]:
+#             if visited[node] == 0:
+#                 stack.append(node)
+#                 visited[node] = 1
+#
+#
+# N, M = map(int, input().split())
+# adj_list = [[] for _ in range(N+1)]
+# visited = [0]*(N+1)
+# connected_cnt = 0
+# for _ in range(M):
+#     u, v = map(int, input().split())
+#     adj_list[u].append(v)
+#     adj_list[v].append(u)
+# for i in range(1,N+1):
+#     if visited[i] == 0:
+#         dfs(i)
+#         connected_cnt += 1
+#
+# print(connected_cnt)
+
+# re-v2
 def dfs(start):
-    stack = []
-    stack.append(start)
     visited[start] = 1
-
-    while stack:
-        start = stack.pop()
-        for node in adj_list[start]:
-            if visited[node] == 0:
-                stack.append(node)
-                visited[node] = 1
-
+    for node in adj_list[start]:
+        if visited[node] == 0:
+            dfs(node)
+    return
 
 N, M = map(int, input().split())
 adj_list = [[] for _ in range(N+1)]
