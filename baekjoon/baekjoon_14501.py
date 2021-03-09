f71e47f151d3fa1fd50d4e7173596d867c1f9abf
KJW <dnnls1@naver.com>
2021-03-09 19:12:19 +0900

    0309

--- baekjoon/baekjoon_14501.py
+++ baekjoon/baekjoon_14501.py
@@ -1,58 +1,179 @@
 # baekjoon_14501 퇴사
-import sys
+# import sys
+#
+#
+# # v1
+# def time_dfs(time_pi,idx):
+#     stack = []
+#     stack.append(time_pi)
+#     # idx = timetable.index(time_pi)
+#     sum_pi = 0
+#
+#     # v1
+#     # while stack:
+#     #     ti, pi = stack.pop()
+#     #     idx += ti
+#     #     if idx > N:
+#     #         break
+#     #     if (idx-1) < N:
+#     #         sum_pi += pi
+#     #         stack.append(timetable[idx])
+#     # return sum_pi
+#
+#     # v2
+#     # while stack:
+#     #     ti, pi = stack.pop()
+#     #     idx += ti
+#     #     if (idx-1) < N:
+#     #         sum_pi += pi
+#     #     if idx >= N:
+#     #         break
+#     #     else:
+#     #         stack.append(timetable[idx])
+#     # return sum_pi
+#
+#     # v3
+#     while stack:
+#         ti, pi = stack.pop()
+#         idx += ti
+#         if idx < N:
+#             sum_pi += pi
+#         if idx >= N:
+#             return sum_pi
+#         else:
+#             stack.append(timetable[idx])
+#
+#
+# N = int(input())
+# timetable = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(N)]
+#
+# max_pi = 0
+# idx = 0
+# for time_pi in timetable:
+#     pi_tmp = time_dfs(time_pi, idx)
+#     idx += 1
+#     if pi_tmp > max_pi:
+#         max_pi = pi_tmp
+#
+# print(max_pi)
 
-def time_dfs(time_pi,idx):
-    stack = []
-    stack.append(time_pi)
-    # idx = timetable.index(time_pi)
-    sum_pi = 0
 
-    # v1
-    # while stack:
-    #     ti, pi = stack.pop()
-    #     idx += ti
-    #     if idx > N:
-    #         break
-    #     if (idx-1) < N:
-    #         sum_pi += pi
-    #         stack.append(timetable[idx])
-    # return sum_pi
 
-    # v2
-    # while stack:
-    #     ti, pi = stack.pop()
-    #     idx += ti
-    #     if (idx-1) < N:
-    #         sum_pi += pi
-    #     if idx >= N:
-    #         break
-    #     else:
-    #         stack.append(timetable[idx])
-    # return sum_pi
 
-    # v3
-    while stack:
-        ti, pi = stack.pop()
-        idx += ti
-        if idx < N:
-            sum_pi += pi
-        if idx >= N:
-            return sum_pi
-        else:
-            stack.append(timetable[idx])
+# re-v1
+
+# def dfs(start):
+#     stack = []
+#     stack.append(start)
+#     money = table[start][1]
+#
+#     while stack:
+#         start = stack.pop()
+#         next_day = start + table[start][0]
+#         if next_day < N:
+#             if next_day + table[next_day][0] <= N:
+#                 stack.append(next_day)
+#                 money += table[next_day][1]
+#     return money
+#
+#
+# N = int(input())
+# table = []
+#
+# for i in range(N):
+#     ti, pi = map(int, input().split())
+#     table.append([ti, pi])
+#
+# res = 0
+# for i in range(N):
+#     if i+table[i][0] > N:
+#         continue
+#     res = max(res, dfs(i))
+# print(res)
+
+
+# re-v2
+
+# def dfs(start, money):
+#     global res
+#
+#     if start + table[start][0] >= N:
+#         res = max(res, money)
+#         return
+#     else:
+#         money += table[start][1]
+#         dfs(start+table[start][0], money)
+#         dfs(start+1, money-table[start][1])
+#
+#
+# N = int(input())
+# table = []
+#
+# for i in range(N):
+#     ti, pi = map(int, input().split())
+#     table.append([ti, pi])
+#
+# res = 0
+# for i in range(N):
+#     dfs(i,0)
+# print(res)
+
+
+# re -v3
+# def dfs(start, money):
+#     global res
+#
+#     if start + table[start][0] > N:
+#         res = max(res, money)
+#         return
+#
+#     if start + table[start][0] == N:
+#         money += table[start][1]
+#         res = max(res, money)
+#         return
+#
+#     if start + table[start][0] < N:
+#         dfs(start+table[start][0], money+table[start][1])
+#         dfs(start+1, money)
+#
+#
+# N = int(input())
+# table = []
+#
+# for i in range(N):
+#     ti, pi = map(int, input().split())
+#     table.append([ti, pi])
+#
+# res = 0
+# for i in range(N):
+#     dfs(i,0)
+# print(res)
+
+# re-v4
+def dfs(start, money):
+    global res
+
+    if start > N:
+        # res = max(res, money)
+        return
+
+    if start == N:
+        # money += table[start][1]
+        res = max(res, money)
+        return
+
+    if start < N:
+        dfs(start+table[start][0], money+table[start][1])
+        dfs(start+1, money)
 
 
 N = int(input())
-timetable = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(N)]
+table = []
 
-max_pi = 0
-idx = 0
-for time_pi in timetable:
-    pi_tmp = time_dfs(time_pi, idx)
-    idx += 1
-    if pi_tmp > max_pi:
-        max_pi = pi_tmp
+for i in range(N):
+    ti, pi = map(int, input().split())
+    table.append([ti, pi])
 
-print(max_pi)
-
-
+res = 0
+dfs(0,0)
+print(res)
--- baekjoon/baekjoon_16234.py
+++ baekjoon/baekjoon_16234.py
@@ -1,67 +1,67 @@
 # baekjoon_16234  인구이동
 
 # v1
-import collections
-
-
-def bfs(s_i, s_j):
-    dx = [0,-1,0,1]
-    dy = [-1,0,1,0]
-
-    union = []
-    population = countries[s_i][s_j]
-    queue = collections.deque()
-
-    queue.append([s_i, s_j])
-    visited[s_i][s_j] = 1
-    union.append([s_i, s_j])
-    move = False
-
-    while queue:
-        c_i, c_j = queue.popleft()
-
-        for c in range(4):
-            x = c_i + dx[c]
-            y = c_j + dy[c]
-            if 0 <= x < N and 0 <= y < N:
-                if visited[x][y] == 0:
-                    if L <= abs(countries[c_i][c_j]-countries[x][y]) <= R:
-                        move = True
-                        population += countries[x][y]
-                        union.append([x,y])
-                        queue.append([x,y])
-                        visited[x][y] = 1
-
-    union_population = population // len(union)
-
-    for u1, u2 in union:
-        countries[u1][u2] = union_population
-
-    # if len(union)>0:
-    #     move = True
-    return move
-
-
-N, L, R = map(int, input().split())
-countries = [list(map(int, input().split())) for _ in range(N)]
-
-
-cnt = 0
-
-while True:
-    visited = [[0] * N for __ in range(N)]
-    trg = True
-    for i in range(N):
-        for j in range(N):
-            if visited[i][j] == 0:
-                moves = bfs(i,j)
-                if moves:
-                    trg = False
-
-    if trg:
-        break
-    cnt += 1
-print(cnt)
+# import collections
+#
+#
+# def bfs(s_i, s_j):
+#     dx = [0,-1,0,1]
+#     dy = [-1,0,1,0]
+#
+#     union = []
+#     population = countries[s_i][s_j]
+#     queue = collections.deque()
+#
+#     queue.append([s_i, s_j])
+#     visited[s_i][s_j] = 1
+#     union.append([s_i, s_j])
+#     move = False
+#
+#     while queue:
+#         c_i, c_j = queue.popleft()
+#
+#         for c in range(4):
+#             x = c_i + dx[c]
+#             y = c_j + dy[c]
+#             if 0 <= x < N and 0 <= y < N:
+#                 if visited[x][y] == 0:
+#                     if L <= abs(countries[c_i][c_j]-countries[x][y]) <= R:
+#                         move = True
+#                         population += countries[x][y]
+#                         union.append([x,y])
+#                         queue.append([x,y])
+#                         visited[x][y] = 1
+#
+#     union_population = population // len(union)
+#
+#     for u1, u2 in union:
+#         countries[u1][u2] = union_population
+#
+#     # if len(union)>0:
+#     #     move = True
+#     return move
+#
+#
+# N, L, R = map(int, input().split())
+# countries = [list(map(int, input().split())) for _ in range(N)]
+#
+#
+# cnt = 0
+#
+# while True:
+#     visited = [[0] * N for __ in range(N)]
+#     trg = True
+#     for i in range(N):
+#         for j in range(N):
+#             if visited[i][j] == 0:
+#                 moves = bfs(i,j)
+#                 if moves:
+#                     trg = False
+#
+#     if trg:
+#         break
+#     cnt += 1
+# print(cnt)
 
 
 # v2
@@ -133,3 +133,117 @@ print(cnt)
 #         break
 #     cnt += 1
 # print(cnt)
+
+
+# re-v1
+# from collections import deque
+#
+# def bfs(s_i, s_j):
+#     queue = deque()
+#     queue.append([s_i, s_j])
+#     visited[s_i][s_j] = 1
+#
+#     connection = 0
+#     total = world[s_i][s_j]
+#     contries = [[s_i,s_j]]
+#     while queue:
+#         s_i, s_j = queue.popleft()
+#         for c in range(4):
+#             x = s_i + dx[c]
+#             y = s_j + dy[c]
+#             if 0 <= x < N and 0 <= y < N and visited[x][y] == 0:
+#                 if L <= abs(world[s_i][s_j] - world[x][y]) <= R:
+#                     connection += 1
+#                     total += world[x][y]
+#                     contries.append([x,y])
+#                     queue.append([x,y])
+#                     visited[x][y] = 1
+#     if connection:
+#         moving_num = total // (connection+1)
+#         for c_i, c_j in contries:
+#             world[c_i][c_j] = moving_num
+#
+#         return True
+#     return False
+#
+#
+# N, L, R = map(int,input().split())
+#
+# world = [list(map(int, input().split())) for _ in range(N)]
+#
+# dx = [0,-1,0,1]
+# dy = [-1,0,1,0]
+#
+#
+# res = 0
+# while True:
+#     visited = [[0]*N for _ in range(N)]
+#     trg2 = False
+#     for i in range(N):
+#         for j in range(N):
+#             if visited[i][j] == 0:
+#                 trg1 = bfs(i,j)
+#                 if trg1:
+#                     res += 1
+#                     trg2 = True
+#     if not trg2:
+#         break
+# print(res)
+
+
+# re-v2
+
+from collections import deque
+
+def bfs(s_i, s_j):
+    queue = deque()
+    queue.append([s_i, s_j])
+    visited[s_i][s_j] = 1
+
+    connection = 0
+    total = world[s_i][s_j]
+    contries = [[s_i,s_j]]
+    while queue:
+        s_i, s_j = queue.popleft()
+        for c in range(4):
+            x = s_i + dx[c]
+            y = s_j + dy[c]
+            if 0 <= x < N and 0 <= y < N and visited[x][y] == 0:
+                if L <= abs(world[s_i][s_j] - world[x][y]) <= R:
+                    connection += 1
+                    total += world[x][y]
+                    contries.append([x,y])
+                    queue.append([x,y])
+                    visited[x][y] = 1
+    if connection:
+        moving_num = total // (connection+1)
+        for c_i, c_j in contries:
+            world[c_i][c_j] = moving_num
+
+        return True
+    return False
+
+
+N, L, R = map(int,input().split())
+
+world = [list(map(int, input().split())) for _ in range(N)]
+
+dx = [0,-1,0,1]
+dy = [-1,0,1,0]
+
+
+res = 0
+while True:
+    visited = [[0]*N for _ in range(N)]
+    trg2 = False
+    for i in range(N):
+        for j in range(N):
+            if visited[i][j] == 0:
+                trg1 = bfs(i,j)
+                if trg1:
+                    trg2 = True
+    if trg2:
+        res += 1
+    else:
+        break
+print(res)
