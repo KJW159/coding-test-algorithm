2af421afc72379fb58bd1acfc0018404a00e934a
KJW <dnnls1@naver.com>
2021-03-13 02:58:38 +0900

    0312-v2

--- baekjoon/baekjoon_17281.py
+++ baekjoon/baekjoon_17281.py
@@ -102,32 +102,96 @@
 # print(res)
 
 # v3
-from itertools import permutations
+# from itertools import permutations
+#
+# def playing_game(team, score, start_player):
+#     out_cnt = 0
+#     score_tmp = 0
+#     b1, b2, b3 = 0, 0, 0
+#     while True:
+#         start_player = (start_player+1) % 9
+#         play = score[team[start_player]]
+#         if play == 0:
+#             out_cnt += 1
+#             if out_cnt == 3:
+#                 last_player = start_player
+#                 return score_tmp, last_player
+#         if play == 1:
+#             score_tmp += b3
+#             b1, b2, b3 = 1, b1, b2
+#         if play == 2:
+#             score_tmp += (b2 + b3)
+#             b1, b2, b3 = 0,1,b1
+#         if play == 3:
+#             score_tmp += (b1 + b2 + b3)
+#             b1, b2, b3 = 0, 0, 1
+#         if play == 4:
+#             score_tmp += (b1 + b2 + b3 + 1)
+#             b1, b2, b3, = 0, 0, 0
+#
+# N = int(input())
+# res = 0
+# start_player_tmp = 0
+# scores = [list(map(int, input().split())) for _ in range(N)]
+# players = list(range(1, 9))
+# for team in permutations(players):
+#     team = list(team)
+#     team.insert(3, 0)
+#     score_max = 0
+#     last_player = -1
+#     for i in range(N):
+#         score = scores[i]
+#         score_tmp, last_player = playing_game(team, score, last_player)
+#         score_max += score_tmp
+#     res = max(res, score_max)
+#
+# print(res)
 
-def playing_game(team, score, start_player):
-    out_cnt = 0
-    score_tmp = 0
-    b1, b2, b3 = 0, 0, 0
-    while True:
-        start_player = (start_player+1) % 9
-        play = score[team[start_player]]
-        if play == 0:
-            out_cnt += 1
-            if out_cnt == 3:
-                last_player = start_player
-                return score_tmp, last_player
-        if play == 1:
-            score_tmp += b3
-            b1, b2, b3 = 1, b1, b2
-        if play == 2:
-            score_tmp += (b2 + b3)
-            b1, b2, b3 = 0,1,b1
-        if play == 3:
-            score_tmp += (b1 + b2 + b3)
-            b1, b2, b3 = 0, 0, 1
-        if play == 4:
-            score_tmp += (b1 + b2 + b3 + 1)
-            b1, b2, b3, = 0, 0, 0
+
+# v4
+# from itertools import permutations
+#
+# N = int(input())
+# res = 0
+# start_player_tmp = 0
+# scores = [list(map(int, input().split())) for _ in range(N)]
+# players = list(range(1, 9))
+# for team in permutations(players):
+#     team = list(team)
+#     team.insert(3, 0)
+#     score_max = 0
+#     start_player = -1
+#     for i in range(N):
+#         score = scores[i]
+#         out_cnt = 0
+#         score_tmp = 0
+#         b1, b2, b3 = 0, 0, 0
+#         while True:
+#             start_player = (start_player + 1) % 9
+#             play = score[team[start_player]]
+#             if play == 0:
+#                 out_cnt += 1
+#                 if out_cnt == 3:
+#                     break
+#             if play == 1:
+#                 score_tmp += b3
+#                 b1, b2, b3 = 1, b1, b2
+#             if play == 2:
+#                 score_tmp += (b2 + b3)
+#                 b1, b2, b3 = 0, 1, b1
+#             if play == 3:
+#                 score_tmp += (b1 + b2 + b3)
+#                 b1, b2, b3 = 0, 0, 1
+#             if play == 4:
+#                 score_tmp += (b1 + b2 + b3 + 1)
+#                 b1, b2, b3, = 0, 0, 0
+#         score_max += score_tmp
+#     res = max(res, score_max)
+# print(res)
+
+
+#v5
+from itertools import permutations
 
 N = int(input())
 res = 0
@@ -136,13 +200,33 @@ scores = [list(map(int, input().split())) for _ in range(N)]
 players = list(range(1, 9))
 for team in permutations(players):
     team = list(team)
-    team.insert(3, 0)
+    team = team[:3] + [0] + team[3:]
     score_max = 0
-    last_player = -1
+    start_player = -1
     for i in range(N):
         score = scores[i]
-        score_tmp, last_player = playing_game(team, score, last_player)
+        out_cnt = 0
+        score_tmp = 0
+        b1, b2, b3 = 0, 0, 0
+        while True:
+            start_player = (start_player + 1) % 9
+            play = score[team[start_player]]
+            if play == 0:
+                out_cnt += 1
+                if out_cnt == 3:
+                    break
+            if play == 1:
+                score_tmp += b3
+                b1, b2, b3 = 1, b1, b2
+            if play == 2:
+                score_tmp += (b2 + b3)
+                b1, b2, b3 = 0, 1, b1
+            if play == 3:
+                score_tmp += (b1 + b2 + b3)
+                b1, b2, b3 = 0, 0, 1
+            if play == 4:
+                score_tmp += (b1 + b2 + b3 + 1)
+                b1, b2, b3, = 0, 0, 0
         score_max += score_tmp
     res = max(res, score_max)
-
 print(res)
