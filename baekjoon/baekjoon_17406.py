649fcfcebd58966c7be00fdebfd6e16d8c2f4021
KJW <dnnls1@naver.com>
2021-03-13 20:54:10 +0900

    0313-v2

--- baekjoon/baekjoon_17406.py
+++ baekjoon/baekjoon_17406.py
@@ -30,8 +30,49 @@
 #             nx += 1
 
 # v2
-from itertools import permutations
+# from itertools import permutations
+# import math
+#
+#
+# N, M, K = map(int, input().split())
+#
+# arr = [list(map(int, input().split())) for _ in range(N)]
+# rotations = []
+# res = math.inf
+# for _ in range(K):
+#     r,c,s = map(int, input().split())
+#     rotations.append([r-1, c-1, s])
+#
+# for rotation in permutations(rotations):
+#     part_tmp = [arr[i][:] for i in range(N)]
+#     for cal in rotation:
+#         r, c, s = cal
+#         # 한칸씩 회전
+#         for n in range(s, 0, -1):
+#             tmp = part_tmp[r-n][c+n]
+#             part_tmp[r-n][c-n+1:c+n+1] = part_tmp[r-n][c-n:c+n]
+#             for x in range(r-n, r+n):
+#                 part_tmp[x][c-n] = part_tmp[x+1][c-n]
+#             part_tmp[r+n][c-n:c+n] = part_tmp[r+n][c-n+1:c+n+1]
+#             for x in range(r+n, r-n, -1):
+#                 part_tmp[x][c+n] = part_tmp[x-1][c+n]
+#             part_tmp[r-n+1][c+n] = tmp
+#
+#     for row in part_tmp:
+#         res = min(res, sum(row))
+#
+# print(res)
+
+
+# v3
 import math
+def permutations1(arr, r):
+    for i in range(len(arr)):
+        if r == 1:
+            yield [arr[i]]
+        else:
+            for next in permutations1(arr[:i]+arr[i+1:], r-1):
+                yield [arr[i]] + next
 
 
 N, M, K = map(int, input().split())
@@ -43,7 +84,7 @@ for _ in range(K):
     r,c,s = map(int, input().split())
     rotations.append([r-1, c-1, s])
 
-for rotation in permutations(rotations):
+for rotation in permutations1(rotations, K):
     part_tmp = [arr[i][:] for i in range(N)]
     for cal in rotation:
         r, c, s = cal
@@ -62,5 +103,4 @@ for rotation in permutations(rotations):
         res = min(res, sum(row))
 
 print(res)
-
 
