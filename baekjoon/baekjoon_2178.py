b15da4140b06971d6ddc41aec62a3244e2691916
KJW <dnnls1@naver.com>
2021-03-23 15:19:06 +0900

    0323

--- baekjoon/baekjoon_2178.py
+++ baekjoon/baekjoon_2178.py
@@ -27,3 +27,32 @@ arr = [list(map(int, input())) for _ in range(N)]
 visited = [[0]*M for _ in range(N)]
 distance_min = bfs(N,M)
 print('{}'.format(distance_min))
+
+
+# re-v1
+# from collections import deque
+#
+# def bfs():
+#     queue = deque()
+#     queue.append([0,0])
+#     arr[0][0] = 2
+#
+#     while queue:
+#         s_i, s_j = queue.popleft()
+#         if s_i == N-1 and s_j == M-1:
+#             return arr[s_i][s_j]-1
+#         for c in range(4):
+#             x = s_i + dx[c]
+#             y = s_j + dy[c]
+#             if 0 <= x < N and 0 <= y < M:
+#                 if arr[x][y] == 1:
+#                     queue.append([x,y])
+#                     arr[x][y] = arr[s_i][s_j]+1
+#
+# N,M = map(int, input().split())
+#
+# arr = [list(map(int, input())) for _ in range(N)]
+# dx = [0,-1,0,1]
+# dy = [-1,0,1,0]
+# res = bfs()
+# print(res)
