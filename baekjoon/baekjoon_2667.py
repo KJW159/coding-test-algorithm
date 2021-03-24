5268944c9666b3d920f32ba948c73077bac3bb97
KJW <dnnls1@naver.com>
2021-03-24 17:27:22 +0900

    0324

--- baekjoon/baekjoon_2667.py
+++ baekjoon/baekjoon_2667.py
@@ -1,45 +1,35 @@
-# baekjoon_2667 단지 번호 붙이기
-
+# baekjoon_2667 단지번호붙이기
 
 def dfs(i,j):
-    dx = [0, -1, 0, 1]
-    dy = [-1, 0, 1, 0]
-
-    stack = []
-    stack.append([i,j])
-    cnt = 1
-
+    stack = [[i,j]]
+    arr[i][j] = 2
+    cnt_tmp = 1
     while stack:
-        house = stack.pop()
+        s_i, s_j = stack.pop()
         for c in range(4):
-            x = house[0] + dx[c]
-            y = house[1] + dy[c]
-            if 0 <= x < N and 0 <= y < N:
-                if arr[x][y] == 1 and visited[x][y] == 0:
-                    stack.append([x, y])
-                    visited[x][y] = 1
-                    cnt += 1
-                else:
-                    visited[x][y] = 1
-    return cnt
+            x = s_i + dx[c]
+            y = s_j + dy[c]
+            if 0<=x<N and 0<=y<N:
+                if arr[x][y] == 1:
+                    stack.append([x,y])
+                    arr[x][y] = 2
+                    cnt_tmp += 1
+    return cnt_tmp
 
 N = int(input())
-
 arr = [list(map(int, input())) for _ in range(N)]
+res_num = 0
+res_nums = []
 
-visited = [[0]*N for _ in range(N)]
-house_nums = []
+dx = [0,-1,0,1]
+dy = [-1,0,1,0]
 
 for i in range(N):
     for j in range(N):
-        if arr[i][j] == 1 and visited[i][j] == 0:
-            visited[i][j] = 1
-            house_num = dfs(i,j)
-            house_nums.append(house_num)
-        else:
-            visited[i][j] = 1
-
-house_nums.sort()
-print(len(house_nums))
-for num in house_nums:
-    print(num)
+        if arr[i][j] == 1:
+            res_nums.append(dfs(i,j))
+            res_num += 1
+res_nums.sort()
+print(res_num)
+for k in range(res_num):
+    print(res_nums[k])
