9bb77e18ebcc22209cb65be2ca344833a8ff444a
KJW <dnnls1@naver.com>
2021-03-29 20:58:31 +0900

    0329 cpp

---
+++ cpp/baekjoon/baekjoon_15552.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    std::cin.tie(NULL);
+    std::ios_base::sync_with_stdio(false);
+
+    int T,a,b;
+    std::cin >> T;
+    for(int i=0; i<T; ++i){
+        std::cin >> a >> b;
+        std::cout << a+b << '\n';
+    }
+}
