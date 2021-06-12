16ddec4292fbfae6b382b551dddb080eb175cab7
KJW <dnnls1@naver.com>
2021-06-12 15:36:45 +0900

    0612 cpp

---
+++ cpp/baekjoon/baekjoon_11279.cpp
@@ -0,0 +1,93 @@
+// v1
+// #include <iostream>
+// #include <queue>
+
+// using namespace std;
+
+// int main(){
+
+//     int N, num;
+//     priority_queue<int> pq;
+//     cin >> N;
+//     for (int i=0; i<N; ++i){
+//         cin >> num;
+//         if (num == 0){
+//             if (pq.empty()){
+//                 cout << 0 << '\n';
+//             }
+//             else{
+//                 cout << pq.top() << '\n';
+//                 pq.pop();
+//             }
+//         }
+//         else{
+//             pq.push(num);
+//         }
+//     }
+// }
+
+// v2
+
+// #include <iostream>
+// #include <queue>
+
+// using namespace std;
+
+// int main(){
+//     cin.tie(NULL);
+//     ios_base::sync_with_stdio(false);
+    
+//     int N, num;
+//     priority_queue<int> pq;
+//     cin >> N;
+//     for (int i=0; i<N; ++i){
+//         cin >> num;
+//         if (num == 0){
+//             if (pq.empty()){
+//                 cout << 0 << '\n';
+//             }
+//             else{
+//                 cout << pq.top() << '\n';
+//                 pq.pop();
+//             }
+//         }
+//         else{
+//             pq.push(num);
+//         }
+//     }
+// }
+
+
+// re-v1
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+
+int main(){
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+
+    int N, num;
+    
+    priority_queue<int> pq;
+    cin >> N;
+
+    for (int i=0; i<N; ++i){
+        cin >> num;
+        if (num !=0){
+            pq.push(num);
+        }
+        else{
+            if (pq.empty()){
+                cout << 0 << '\n';
+            }
+            else{
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+        }
+    }
+}
