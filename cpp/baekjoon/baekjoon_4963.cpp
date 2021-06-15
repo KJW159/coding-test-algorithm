7ea4ead2b58a35b43177cf896a7fd7f0610e374d
KJW <dnnls1@naver.com>
2021-06-15 22:23:42 +0900

    0615 cpp

---
+++ cpp/baekjoon/baekjoon_4963.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int dx[8] = {0, -1, -1, -1, 0, 1, 1, 1};
+int dy[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+
+int main(){
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    
+
+}
